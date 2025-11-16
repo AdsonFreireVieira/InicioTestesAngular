905a28d5ac0b89e340ed198ec0f08bc6dc234d91
Adson <adsonfr.vieira@hotmail.com>
2025-11-15 23:53:10 -0300

    atualizacao

--- calculadoraAngula/src/app/app.ts
+++ calculadoraAngula/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import {Calculadora} from './calculadora/calculadora'
 import { RouterOutlet } from '@angular/router';
+import { Calculadora } from "./calculadora/calculadora";
 
 @Component({
   selector: 'app-root',
-  imports: [Calculadora,RouterOutlet],
+  imports: [RouterOutlet, Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('calculadora');
+  protected readonly title = signal('calculadoraAngula');
 }
