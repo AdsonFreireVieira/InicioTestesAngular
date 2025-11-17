b57d00aae184b24cf93d9468ff9f925f19c39182
Adson <adsonfr.vieira@hotmail.com>
2025-11-16 21:39:14 -0300

    add : Form Listas

--- calculadoraAngula/src/app/app.ts
+++ calculadoraAngula/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Calculadora } from "./calculadora/calculadora";
+import { ListasCompras } from './listas-compras/listas-compras';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Calculadora],
+  imports: [RouterOutlet, Calculadora,ListasCompras],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
