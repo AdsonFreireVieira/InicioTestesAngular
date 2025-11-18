33c222cf52eef9ac8fd50f9ea805ca487051ecdb
Adson <adsonfr.vieira@hotmail.com>
2025-11-18 05:15:52 -0300

    listas

--- calculadoraAngula/src/app/listas-compras/listas-compras.ts
+++ calculadoraAngula/src/app/listas-compras/listas-compras.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import {ItemLista} from './itemlista'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-listas-compras',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './listas-compras.html',
   styleUrl: './listas-compras.scss',
 })
 export class ListasCompras {
+item: string='';
+ lista: ItemLista[]=[];
+adicionarItem(){
+let  itemlista = new ItemLista();
+itemlista.nome = this.item
+itemlista.id = this.lista.length +1;
 
+this.lista.push(itemlista);
+
+this.item ='';
+console.table(this.lista)
+}
 }
